d794c893257907270500459c36ee948e6890ef0e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 06:15:21 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,11 +3,18 @@ require_relative "./student.rb"
 class ChattyStudent < Student
   def hello
     super
-    
-    
   end
 
   def raise_hand
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
     super
   end
 end
